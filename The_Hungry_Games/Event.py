a1ca19430403b059caac886050e79406b57d53cc
CorpulentCroc <49886827+CorpulentCroc@users.noreply.github.com>
2020-08-29 20:19:38 -0400

    Added most of battle_helper()
    I added the almost-finished battle_helper() function and tweaked a few other things

--- The_Hungry_Games/Event.py
+++ The_Hungry_Games/Event.py
@@ -12,6 +12,27 @@ class Event:
         self.funct = funct
         
 watching_two_players_battle = Event('Coming across two players battling', 'universal', 'You see two players battling each other and can either fight the winner or leave')
+def battle_helper(p, rng_func, l):
+    opponent = rng_func(l)
+    print(f'A battle with {opponent.name} has begun!')
+    if not p.has_weapon() and not opponent.has_weapon():
+        print('Neither of you have weapons, so the battle ended in a draw.')
+    elif not p.has_weapon():
+        print('You don\'t have a weapon, and thus lost.')
+        return 'loss'
+    elif not opponent.has_weapon():
+        print('Your opponent doesn\'t have a weapon, and thus lost.')
+        return opponent
+    while True:
+        try:
+            weapon_num_choice = int(input(f'Choose a valid weapon from your inventory to fight with: {p.print_weapons()}: '))
+            assert p.inventory[weapon_num_choice-1]._type == 'Weapon'
+            weapon_choice = p.inventory[weapon_num_choice-1]
+        except:
+            print('Enter your input as a valid integer')
+        break
+    #To be implemented
+        
 battle = Event('Battle', 'universal', 'You are confronted by a player a battle ensues')
 back_turned = Event('Coming across a player whose back is turned', 'universal', 'You come across a player unaware of your presence, and can either sneak attack them or leave')
 def find_random_item_helper(player, map_biome):
@@ -28,3 +49,7 @@ def find_random_item_helper(player, map_biome):
         except:
             print("Invalid input")
 find_random_item = Event("Find random item", 'universal', 'You found a random item! Want to pick it up?', find_random_item_helper)
+#Not yet working \/
+def random_player_eliminated_helper():
+    pass
+random_player_eliminated = Event('Random player eliminated', 'universal', 'A random player was eliminated.', random_player_eliminated_helper)
